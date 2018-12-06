c100650f0a0dfa1e45525308071773fb4638a5da
Sudate <e175754@ie.u-ryukyu.ac.jp>
2018-12-06 10:45:05 +0900

    Initial Commit

---
+++ mycallender/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  mycallender
+//
+//  Created by kentarou sudate on 2018/12/06.
+//  Copyright © 2018 kentarou sudate. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
