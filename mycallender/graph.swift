2a20e62d0959de5b4f8f8f1a27e30355d11eee5d
Sudate <e175754@ie.u-ryukyu.ac.jp>
2018-12-06 11:00:03 +0900

    class adds

---
+++ mycallender/graph.swift
@@ -0,0 +1,30 @@
+//
+//  graph.swift
+//  mycallender
+//
+//  Created by kentarou sudate on 2018/12/06.
+//  Copyright © 2018 kentarou sudate. All rights reserved.
+//
+
+import UIKit
+
+class graph: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
